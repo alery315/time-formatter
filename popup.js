f3933de79f48b3a356e4ff386da0813fc93d9e36
shanantong <shanantong@yuanfudao.com>
2025-01-20 16:10:13 +0800

    MOD:弹出时间戳时区修正

--- popup.js
+++ popup.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // 替换示例时间和时间戳，交换它们的位置并加入换行
   info.innerHTML = `
     <p>双击网页中的时间戳(毫秒或秒)会自动显示对应的格式化时间。</p>
-    <p>格式示例：<br><br><strong>${timestamp}</strong></p>
+    <p>格式示例：<br><br><strong>${now.getTime()}</strong></p>
     <p>双击网页中的格式化时间会自动显示对应的时间戳(毫秒)。</p>
     <p>格式示例：<br><br><strong>${formattedTime}</strong></p>
     <p>记得点击按钮复制到剪贴板哦！</p>
